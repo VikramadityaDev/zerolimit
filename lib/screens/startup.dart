a4a9bd54d20d399f58127815a9d55d86ca482aad
VIKRAMADITYA <vikramadityapratap35@gmail.com>
2025-06-04 20:44:13 +0530

    updated

--- lib/screens/startup.dart
+++ lib/screens/startup.dart
@@ -1,4 +1,18 @@
 import 'package:flutter/material.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+import 'package:zerolimit/screens/bottom_bar.dart';
+import 'package:zerolimit/screens/login_scr.dart';
+
+Future<void> setString(String value) async {
+  final prefs = await SharedPreferences.getInstance();
+  prefs.setString('string', value);
+}
+
+Future<String> getString() async {
+  final prefs = await SharedPreferences.getInstance();
+  final value = prefs.getString('string') ?? '';
+  return value;
+}
 
 class StartUp extends StatefulWidget {
   const StartUp({super.key});
@@ -8,8 +22,26 @@ class StartUp extends StatefulWidget {
 }
 
 class _StartUpState extends State<StartUp> {
+  bool isLoading = true;
+  String loginToken = '';
+
+  @override
+  void initState() {
+    super.initState();
+    /// Call the asynchronous method in initState
+    getString().then((value) {
+      setState(() {
+        loginToken = value;
+        isLoading = false;
+      });
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+        home: loginToken == '' ? LoginScreen() : BottomBar()
+    );
   }
 }
