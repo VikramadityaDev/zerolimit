407097fef407bb5084d893760d32da677f1e1c69
VIKRAMADITYA <vikramadityapratap35@gmail.com>
2026-01-03 21:42:01 +0530

    updated model

--- lib/models/uploaded_file.dart
+++ lib/models/uploaded_file.dart
@@ -19,14 +19,12 @@ class UploadedFile extends HiveObject {
   @HiveField(4)
   final String type;
 
-  // keep base64 OPTIONAL (for backward compatibility)
   @HiveField(5)
   final String? thumbnail;
 
   @HiveField(6)
   final String? caption;
 
-  // ✅ NEW: persisted thumbnail cache
   @HiveField(7)
   final Uint8List? thumbBytes;
 
